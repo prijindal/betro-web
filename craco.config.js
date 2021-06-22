2da92e54089aac7c1528c865ca3ffc32358bd4d0
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2021-06-23 03:59:12 +0530

    Added br compression

--- craco.config.js
+++ craco.config.js
@@ -1,8 +1,30 @@
+const zlib = require("zlib");
+const CompressionPlugin = require("compression-webpack-plugin");
+
 // craco.config.js
 module.exports = {
     style: {
         postcss: {
             plugins: [require("tailwindcss"), require("autoprefixer")],
+        },
+    },
+    webpack: {
+        plugins: {
+            add: [
+                new CompressionPlugin({
+                    filename: "[path][base].br",
+                    algorithm: "brotliCompress",
+                    test: /\.(js|css|html|svg)$/,
+                    compressionOptions: {
+                        params: {
+                            [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+                        },
+                    },
+                    threshold: 10240,
+                    minRatio: 0.8,
+                    deleteOriginalAssets: false,
+                }),
+            ],
         },
     },
 };
